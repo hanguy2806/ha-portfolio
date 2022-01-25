48062e16e61f38de1453bce5b51fc3a8bbba66cd
Hoang Tran <hoangtran194@gmail.com>
2022-01-24 16:20:44 -0800

    fixed styles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,11 +17,12 @@ const About = () => {
         <p className="about-description">
           While at Centennial College, I was lucky to join 3 Co-op placements at
           start-up and midsize team at Cya.Live, WIMTACH and Pearson Canada Inc
-          respectively. Always I disciline myself to get good score at school
-          (GPA 4.26/4.50) as well as expand my horizon at programming whenever I
-          have time. My friends think I am a fast learner and team player. I
-          feel myself more intuitive on web development and want to pursue this
-          career path in future.
+          respectively, gaining plenty of experiences through different
+          responsibilities. Always, I discipline myself to get good score at
+          college (GPA 4.26/4.50) as well as expand my horizon at programming
+          whenever I have time. My friends think I am a fast learner and team
+          player. I feel myself more intuitive on web development and want to
+          pursue this career path in future.
         </p>
         <div className="about-wimtach">
           <a
